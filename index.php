e60dccfb11b23e0af22a3841227934ae7dcd910a
cihanproxy <40439612+cihanproxy@users.noreply.github.com>
2020-05-16 00:45:30 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,1 @@
+<iframe width="560" height="315" src="https://www.youtube.com/embed/fbYeayYMDSU?start=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> <br>
